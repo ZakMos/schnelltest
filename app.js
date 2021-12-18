5638d54b613c8b6e0ba2a22cbf0bf77f4349382a
zakmos <z.zza@hotmail.com>
2021-12-18 18:26:08 +0100

    added nodemaile with auth

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+
 const express = require('express');
 const bodyParser = require ('body-parser');
 const { engine }  = require('express-handlebars');
@@ -23,6 +27,55 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
     res.render('contact')
 });
+
+app.post('/send', (req, res) => {
+    const output = `
+        <p> You have a new contact request </p>
+        <h3> Contact Details</h3>
+        <ul>
+            <li>Vorname: ${req.body.fname}</li>
+            <li>Nachname: ${req.body.lname}</li>
+            <li>E-Mail: ${req.body.email}</li>
+            <li>Phone: ${req.body.phone}</li>
+        </ul>
+        <h3>Message
+            ${req.body.message}
+        </h3>
+    `;
+
+    let transporter = nodemailer.createTransport({
+      host: process.env.HOST_NAME,
+      port: process.env.HOST_PORT,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.USER_NAME, 
+        pass: process.env.USER_AUTH, 
+      },
+    //   tls:{
+    //     rejectUnauthorized:false       // only in localhost
+    //   }
+    });
+  
+    // send mail with defined transport object
+    let mailOptions = {
+      from: "Schnelltest " + process.env.EMAIL_NAME, // sender address
+      to: `${req.body.email}`, // list of receivers
+      bcc: process.env.EMAIL_NAME,
+      subject: `Hello ${req.body.fname}  ${req.body.lname} ✔`, // Subject line
+      text: "Hello world?", // plain text body
+      html: output, // html body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Mesage sent: %s', info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        res.render('contact', {msg: 'Email has been sent'})
+    })
+})
+
 
 const port = 3000;
 app.listen(port, () => console.log(`Server start at ${port}`))
